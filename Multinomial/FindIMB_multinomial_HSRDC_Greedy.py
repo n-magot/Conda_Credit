5fcf62e1a8ee8270e0a617cb69a58ec276060f47
n-magot <57372565+n-magot@users.noreply.github.com>
2025-10-29 15:32:01 +0200

    Update FindIMB_multinomial_HSRDC_Greedy.py

--- Multinomial/FindIMB_multinomial_HSRDC_Greedy.py
+++ Multinomial/FindIMB_multinomial_HSRDC_Greedy.py
@@ -2,8 +2,8 @@
 The code uses multinomial-Dirichlet distributions for binary and categorical data with more than two or three categories, offering closed-form 
 marginal solutions. Instead of an exhaustive search, we use a greedy search method. This approach is applied to find the sets in Dₒ, Dₑ, and (Dₒ + Dₑ) 
 that produce the best Dₒ-based, Dₑ-based, and (Dₒ + Dₑ)-based estimators. For FindIMB, we score the best set based on P(De|Do, HZc) (Eq.5 for HZc from 
-the UAI paper), and for the other estimators, we score based on the  marginal probability of Do, De, and (Do+De) for each set: P(Do|HZ_not_c), P(De|HZ_not_c)
-and P(Do, De|HZ_not_c) (Eq.5 for HZ_not_c from UAI paper, second row of the table S1 adjusted)
+the UAI paper), and for the other estimators, we score based on the  marginal probability of Do, De, and (Do+De) for each set: P(Do|Z), P(De|Z)
+and P(Do, De|Z) (Eq.5 for HZ_not_c from UAI paper, second row of the table S1 adjusted)
 
 For each of the 20 runs, we also report the three sets that maximize either P(HZc|Do, De) or P(HZ_not_c|Do, De).
 For the De_test cases, we calculate the fraction of patients who received A1 and had a good outcome (PONV = 0) and report the average of this value. 
